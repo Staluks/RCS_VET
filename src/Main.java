81309405e1c3142aefa8fa0d6b1b8c2a332ccc7f
tatjjako <tatjana.jakona@gmail.com>
2022-08-23 21:13:51 +0300

    - DBConnection class - DBLogic_Clinic class: method "register" - Main: new clinic registration to test DB Connection - MD5 class: to hash password during registration and login

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,35 @@ import java.awt.event.ActionListener;
 public class Main {
     public static void main(String[] args) {
 
-    LogIn loginmeth = new LogIn();
-    Registration regmeth = new Registration();
+//    LogIn loginmeth = new LogIn();
+//    Registration regmeth = new Registration();
+//
+//    loginmeth.loginWindow();
+//
+//    //to join  the panellogin and panelregistration actionlistener is brought to the main class
+//        //if user presses create new account a new window will open with the registration form
+//    loginmeth.regbut.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                loginmeth.panellogin.setVisible(false);
+//                loginmeth.frame.add(regmeth.panelRegistration);
+//                regmeth.registrationWindow();
+//            }
+//        });
+//    //if user decides to not create new account, user can press button back and return to the login page
+//    regmeth.back.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                regmeth.panelRegistration.setVisible(false);
+//                loginmeth.frame.add(loginmeth.panellogin);
+//                loginmeth.loginWindow();
+//            }
+//        });
+//
+//
 
-    loginmeth.loginWindow();
-
-    //to join  the panellogin and panelregistration actionlistener is brought to the main class
-        //if user presses create new account a new window will open with the registration form
-    loginmeth.regbut.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                loginmeth.panellogin.setVisible(false);
-                loginmeth.frame.add(regmeth.panelRegistration);
-                regmeth.registrationWindow();
-            }
-        });
-    //if user decides to not create new account, user can press button back and return to the login page
-    regmeth.back.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                regmeth.panelRegistration.setVisible(false);
-                loginmeth.frame.add(loginmeth.panellogin);
-                loginmeth.loginWindow();
-            }
-        });
-
-
-
-        //DBLogic_Clinic db = new DBLogic_Clinic();
-        //db.register("clName", "cl@email.com", "22030405", "clLegalAddress", "clPhysicalAddress", "clRegNumber", "clUsername", "clPass");
+        DBLogic_Clinic db = new DBLogic_Clinic();
+        db.register("clNameA", "cl2@email.com", "22030406", "cl2LegalAddress", "cl2PhysicalAddress", "cl2RegNumber", "clAUsername", "clAPass");
 
     }
 }
