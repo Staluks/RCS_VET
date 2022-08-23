b74dcf89327eef4e21b78e76def63e2c65d44f8f
tatjjako <tatjana.jakona@gmail.com>
2022-08-23 20:05:03 +0300

    - DBConnection class - DBLogic_Clinic class: method "register" - Main: new clinic registration to test DB Connection

---
+++ src/db/DBLogic_Clinic.java
@@ -0,0 +1,35 @@
+package db;
+
+import java.sql.*;
+
+public class DBLogic_Clinic extends DBConnection{
+
+    // register new clinic
+    public void register (String name, String email, String phone, String legal_address, String physical_address, String reg_number, String username, String password) {
+        try {
+
+            // connection to DB
+            Connection conn = connectToDB();
+
+            // sql statement to execute
+            String sql = "INSERT INTO clinic (name, email, phone, legal_address, physical_address, reg_number, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, phone);
+            ps.setString(4, legal_address);
+            ps.setString(5, physical_address);
+            ps.setString(6, reg_number);
+            ps.setString(7, username);
+            ps.setString(8, password);
+
+            ps.executeUpdate();
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
