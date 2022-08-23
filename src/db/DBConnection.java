5611097203600f367a87d777ea68251f83c9e202
tatjjako <tatjana.jakona@gmail.com>
2022-08-23 21:13:51 +0300

    - DBConnection class - DBLogic_Clinic class: method "register" - Main: new clinic registration to test DB Connection - MD5 class: to hash password during registration and login

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DBConnection {
+public abstract class DBConnection {
     private final String DB = "jdbc:mysql://195.201.232.243:3309/rcs_vet";
     private final String USER = "rcs_vet_user";
     private final String PASS = "An0thrS3crt";
--- src/db/DBLogic_Clinic.java
+++ src/db/DBLogic_Clinic.java
@@ -1,5 +1,7 @@
 package db;
 
+import md5.MD5;
+
 import java.sql.*;
 
 public class DBLogic_Clinic extends DBConnection{
@@ -8,8 +10,14 @@ public class DBLogic_Clinic extends DBConnection{
     public void register (String name, String email, String phone, String legal_address, String physical_address, String reg_number, String username, String password) {
         try {
 
+
+
             // connection to DB
             Connection conn = connectToDB();
+
+            // object for password hashing
+            MD5 passwordHash = new MD5();
+
 
             // sql statement to execute
             String sql = "INSERT INTO clinic (name, email, phone, legal_address, physical_address, reg_number, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
@@ -22,7 +30,7 @@ public class DBLogic_Clinic extends DBConnection{
             ps.setString(5, physical_address);
             ps.setString(6, reg_number);
             ps.setString(7, username);
-            ps.setString(8, password);
+            ps.setString(8, passwordHash.md5(password));
 
             ps.executeUpdate();
             conn.close();
