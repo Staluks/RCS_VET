f25c656c696eec4375247714815cdfd3400dd36b
lindabalode <balode.lindaa@gmail.com>
2022-09-02 12:15:27 +0300

    izmainas rcs02

---
+++ src/ClinicDashBoard.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class ClinicDashBoard extends Fields_clinicDashBoard{
+
+    public void clinicDash (){
+        panelClinicDashB.setVisible(true);
+        panelClinicDashB.setLayout(null);
+        panelClinicDashB.setBackground(Color.LIGHT_GRAY);
+
+
+
+    }
+}
