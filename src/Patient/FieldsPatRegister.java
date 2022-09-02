6fcb363731c677096ca5a64f501ec363f6ba02ae
Staluks <janchukssss@inbox.lv>
2022-09-02 13:43:50 +0300

    _created patient registration form

---
+++ src/Patient/FieldsPatRegister.java
@@ -0,0 +1,34 @@
+package Patient;
+import javax.swing.*;
+
+public class FieldsPatRegister {
+
+    JFrame frame = new JFrame();
+    JPanel panelPatRegistration = new JPanel();
+
+    JButton cancel = new JButton("< Cancel");
+    JButton submitpat = new JButton("Submit");
+    JLabel patRegistration = new JLabel("Create new Patients account here!");
+    JLabel patName = new JLabel("Name of Patient *");
+    JLabel patSpecies = new JLabel("Species of Patient *");
+    JLabel passportNr = new JLabel("Password Nr. of Patient *");
+    JLabel dateofBirth = new JLabel("Date of Birth*");
+    JLabel ownerName = new JLabel("Owner Name *");
+    JLabel ownerSurname = new JLabel("Owner Surname *");
+    JLabel ownerTelNr = new JLabel("Owner Tel Nr.  *");
+
+
+
+    JTextField patNameText = new JTextField();
+    JTextField patSpeciesText = new JTextField();
+    JTextField passportNrText = new JTextField();
+    JTextField dateofBirthText = new JTextField();
+    JTextField ownerNameText = new JTextField();
+    JTextField ownerSurnameText = new JTextField();
+    JTextField ownerTelNrText = new JTextField();
+
+
+
+
+
+}
