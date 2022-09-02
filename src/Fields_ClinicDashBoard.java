a94a67f2bf3c93478b3af4225305b18d0e992cdd
lindabalode <balode.lindaa@gmail.com>
2022-09-02 12:56:07 +0300

    clinicdash board

---
+++ src/Fields_ClinicDashBoard.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Fields_ClinicDashBoard {
+
+    JPanel panelClinicDashB = new JPanel();
+
+    JLabel yourDashB = new JLabel("Your Dash Board");
+
+    JButton logout = new JButton("Logo ut");
+    JButton addNewDoc = new JButton("Add New Doctor");
+    JButton block = new JButton("Block");
+    JButton unblock = new JButton("Unblock");
+    JButton edit = new JButton("Edit");
+
+
+}
