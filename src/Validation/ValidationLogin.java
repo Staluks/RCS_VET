3aaf71ed7b8a22f231c7ea88c9ff7a17a8b5cd48
Laura Fialkovska <laura.fialkovska@gmail.com>
2022-09-08 19:16:00 +0300

    Fixes for: [rcs_12] un [rcs_15]

--- src/Validation/ValidationLogin.java
+++ src/Validation/ValidationLogin.java
@@ -4,22 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidationLogin {
-    public static boolean isValidUsername(String name){
+    public boolean isValidUsername(String name){
 
         String regex = "[a-zA-Z\\d]*";
         Pattern p = Pattern.compile(regex);
-        if (name.length()< 6) {
+        if (name.length() < 6) {
             return false;
         }
         Matcher m = p.matcher(name);
         return m.matches();
     }
 
-    public static boolean isValidPassword(String name){
+    public boolean isValidPassword(String name){
 
         String regex = "[A-Za-z\\d@\u0024\u0021\u0023\u0025\u0026\u002A\u005F\u005E\u007E]+";
         Pattern p = Pattern.compile(regex);
-        if (name.length()<7 || name.length() > 20) {
+        if (name.length() < 8 || name.length() > 20) {
             return false;
         }
         Matcher m = p.matcher(name);
