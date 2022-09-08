dbb06d4ab70291518ed088b57b73ad91fe210b30
Laura Fialkovska <laura.fialkovska@gmail.com>
2022-09-08 20:34:01 +0300

    Fixes for [rcs 18]

--- src/NewMedHistory.java
+++ src/NewMedHistory.java
@@ -1,6 +1,5 @@
-package Patient;
+import Fields.FieldsNewMedHistory;
 
-import javax.swing.*;
 import java.awt.*;
 
 public class NewMedHistory extends FieldsNewMedHistory {
@@ -8,12 +7,10 @@ public class NewMedHistory extends FieldsNewMedHistory {
     public void NewMedHistoryWindow() {
 
         //defining panelPatRegistration window for Patient registration form
-
         panelNewMedHis.setVisible(true);
         panelNewMedHis.setLayout(null);
         panelNewMedHis.setBackground(Color.LIGHT_GRAY);
         panelNewMedHis.add(cancel);
-
 
         //if user wants he can click cancel to return to the first window or submit data
         cancel.setBounds(10, 10, 90, 40);
@@ -30,17 +27,12 @@ public class NewMedHistory extends FieldsNewMedHistory {
         panelNewMedHis.add(manipulations);
         panelNewMedHis.add(medicine);
 
-
-
         patName.setBounds(50, 50, 200,200);
         patSpecies.setBounds(50, 105, 200, 200);
         diagnosis.setBounds(50,160,200,200 );
         dateOfDiagnosis.setBounds(50,225,200,200);
         manipulations.setBounds(50,290,200,200);
         medicine.setBounds(50,365,200,200);
-
-
-
 
         //adding all the fields for user input in the form
         panelNewMedHis.add(patNameText);
@@ -49,8 +41,6 @@ public class NewMedHistory extends FieldsNewMedHistory {
         panelNewMedHis.add(dateOfDiagnosisText);
         panelNewMedHis.add(manipulationsText);
         panelNewMedHis.add(medicineText);
-
-
 
         patNameText.setBounds(280,140,200,35);
         patSpeciesText.setBounds(280,185,200,35);
